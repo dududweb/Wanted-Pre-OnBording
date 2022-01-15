7274afcae0d89dac501fc1b283b0ef3c8d155024
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-15 12:24:43 +0900

    wip: images and svg setting

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,7 +1,32 @@
 import React from "react";
+import SearchIcon from "../../asset/images/nav/searchicon.svg";
+import AlertIcon from "../../asset/images/nav/alerticon.svg";
 
 function Nav() {
-  return <div className="row">123</div>;
+  return (
+    <div className="bg-red-600 flex justify-center w-96 mx-auto">
+      <nav>
+        <div>
+          <h1>logo</h1>
+        </div>
+        <ul>
+          <li>채용</li>
+          <li>채용</li>
+          <li>채용</li>
+          <li>채용</li>
+          <li>채용</li>
+          <li>채용</li>
+          <li>채용</li>
+        </ul>
+      </nav>
+      <div>
+        <img src={SearchIcon} />
+        <img src={AlertIcon} />
+      </div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
 }
 
 export default Nav;
