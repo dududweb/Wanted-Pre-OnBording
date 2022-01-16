160e43ff02d1503b46a43c656c2372f04f92dbe3
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-16 14:52:23 +0900

    wip:nav3

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CATEGORIES from "./category";
 import SearchIcon from "../../asset/images/nav/searchicon.svg";
 import AlertIcon from "../../asset/images/nav/alerticon.svg";
 import Menu from "../../asset/images/nav/icon-menu.png";
@@ -10,23 +11,30 @@ function Nav() {
     <div className="bg-red-600">
       <nav className="flex justify-between items-center max-w-screen-lg mx-auto ">
         <div className="flex justify-between items-center w-36">
-          <img className="w-sm" src={Menu} />
-          <h1>
-            <img className="w-sm" src={Logo} />
+          <img src={Menu} alt="menu" />
+          <h1 className="w-sm">
+            <img src={Logo} alt="logo" />
           </h1>
         </div>
         <ul className="flex">
-          <li>채용</li>
-          <li>이벤트</li>
-          <li>직군별 연봉</li>
-          <li>이력서</li>
-          <li>커뮤니티</li>
-          <li>프리랜서</li>
-          <li>AI 합격 예측</li>
+          {CATEGORIES.map((category_list) => {
+            return (
+              <>
+                <li className="pr-7">{category_list.category}</li>
+              </>
+            );
+          })}
         </ul>
         <div className="flex">
-          <img src={SearchIcon} />
-          <img src={AlertIcon} />
+          <ul className="flex">
+            <li>
+              <img src={SearchIcon} alt="searchicon" />
+            </li>
+            <li>
+              <img src={AlertIcon} alt="alerticon" />
+            </li>
+            <li>기업서비스</li>
+          </ul>
         </div>
       </nav>
     </div>
---
+++ src/component/Nav/category.js
@@ -0,0 +1,32 @@
+const CATEGORIES = [
+  {
+    id: 1,
+    category: "채용",
+  },
+  {
+    id: 2,
+    category: "이벤트",
+  },
+  {
+    id: 3,
+    category: "직군별 연봉",
+  },
+  {
+    id: 4,
+    category: "이력서",
+  },
+  {
+    id: 5,
+    category: "커뮤니티",
+  },
+  {
+    id: 6,
+    category: "프리랜서",
+  },
+  {
+    id: 7,
+    category: "AI 합격 예측",
+  },
+];
+
+export default CATEGORIES;
