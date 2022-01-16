13bbd48da21c6b759a8269b025925069879ec61a
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-16 15:34:01 +0900

    wip:nav4

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -5,37 +5,42 @@ import SearchIcon from "../../asset/images/nav/searchicon.svg";
 import AlertIcon from "../../asset/images/nav/alerticon.svg";
 import Menu from "../../asset/images/nav/icon-menu.png";
 import Logo from "../../asset/images/logo/logoblack.png";
+import Profile from "../../asset/images/nav/profile_default.png";
 
 function Nav() {
   return (
-    <div className="bg-red-600">
-      <nav className="flex justify-between items-center max-w-screen-lg mx-auto ">
+    <div className="">
+      <nav className="flex justify-between items-center max-w-screen-lg mx-auto py-4">
         <div className="flex justify-between items-center w-36">
-          <img src={Menu} alt="menu" />
-          <h1 className="w-sm">
+          <img className="w-6" src={Menu} alt="menu" />
+          <h1 className="w-28">
             <img src={Logo} alt="logo" />
           </h1>
         </div>
         <ul className="flex">
           {CATEGORIES.map((category_list) => {
             return (
-              <>
-                <li className="pr-7">{category_list.category}</li>
-              </>
+              <li className="pr-7" key={category_list.id}>
+                <p>{category_list.category}</p>
+              </li>
             );
           })}
         </ul>
-        <div className="flex">
-          <ul className="flex">
-            <li>
-              <img src={SearchIcon} alt="searchicon" />
-            </li>
-            <li>
-              <img src={AlertIcon} alt="alerticon" />
-            </li>
-            <li>기업서비스</li>
-          </ul>
-        </div>
+        <ul className="flex justify-between items-center">
+          <li>
+            <img src={SearchIcon} alt="searchicon" />
+          </li>
+          <li>
+            <img src={AlertIcon} alt="searchicon" />
+          </li>
+          <li className="rounded-full overflow-hidden">
+            <img className="w-8" src={Profile} alt="profileimg" />
+          </li>
+          <li className="flex border-gray border-solid border rounded-full">
+            <p className="p">|</p>
+            <p className="p-2">기업서비스</p>
+          </li>
+        </ul>
       </nav>
     </div>
   );
