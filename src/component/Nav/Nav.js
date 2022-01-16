992e6067759633d627168b111371ccf1c7b5cc69
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-16 10:17:26 +0900

    wip:nav

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,34 +1,31 @@
 import React from "react";
+
 import SearchIcon from "../../asset/images/nav/searchicon.svg";
 import AlertIcon from "../../asset/images/nav/alerticon.svg";
-import RightArrow from "../../asset/images/nav/rightarrow.svg";
-import LeftArrow from "../../asset/images/nav/leftarrow.svg";
+import Menu from "../../asset/images/nav/icon-menu.png";
 
 function Nav() {
   return (
-    <div className="bg-red-600 flex justify-center w-96 mx-auto">
-      <nav>
+    <div className="bg-red-600 flex justify-center mx-auto">
+      <nav className="flex">
         <div>
+          <img src={Menu} />
           <h1>logo</h1>
         </div>
         <ul>
           <li>채용</li>
-          <li>채용</li>
-          <li>채용</li>
-          <li>채용</li>
-          <li>채용</li>
-          <li>채용</li>
-          <li>채용</li>
+          <li>이벤트</li>
+          <li>직군별 연봉</li>
+          <li>이력서</li>
+          <li>커뮤니티</li>
+          <li>프리랜서</li>
+          <li>AI 합격 예측</li>
         </ul>
+        <div>
+          <img src={SearchIcon} />
+          <img src={AlertIcon} />
+        </div>
       </nav>
-      <div>
-        <img src={SearchIcon} />
-        <img src={AlertIcon} />
-        <img src={RightArrow} />
-        <img src={LeftArrow} />
-      </div>
-      <div>2</div>
-      <div>3</div>
     </div>
   );
 }
