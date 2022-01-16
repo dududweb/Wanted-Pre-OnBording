67662df1692324ff4cac60340069c07a92ea7a4f
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-16 13:53:49 +0900

    wip:nav2

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -3,16 +3,19 @@ import React from "react";
 import SearchIcon from "../../asset/images/nav/searchicon.svg";
 import AlertIcon from "../../asset/images/nav/alerticon.svg";
 import Menu from "../../asset/images/nav/icon-menu.png";
+import Logo from "../../asset/images/logo/logoblack.png";
 
 function Nav() {
   return (
-    <div className="bg-red-600 flex justify-center mx-auto">
-      <nav className="flex">
-        <div>
-          <img src={Menu} />
-          <h1>logo</h1>
+    <div className="bg-red-600">
+      <nav className="flex justify-between items-center max-w-screen-lg mx-auto ">
+        <div className="flex justify-between items-center w-36">
+          <img className="w-sm" src={Menu} />
+          <h1>
+            <img className="w-sm" src={Logo} />
+          </h1>
         </div>
-        <ul>
+        <ul className="flex">
           <li>채용</li>
           <li>이벤트</li>
           <li>직군별 연봉</li>
@@ -21,7 +24,7 @@ function Nav() {
           <li>프리랜서</li>
           <li>AI 합격 예측</li>
         </ul>
-        <div>
+        <div className="flex">
           <img src={SearchIcon} />
           <img src={AlertIcon} />
         </div>
