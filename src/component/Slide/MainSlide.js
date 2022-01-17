d13ad9dddb59e598475a1aae00338275aaa83ca6
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-18 08:16:09 +0900

    wip:slide js2

--- src/component/Slide/MainSlide.js
+++ src/component/Slide/MainSlide.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import LeftArrow from "../../asset/images/slide/leftarrow.svg";
 import RightArrow from "../../asset/images/slide/rightarrow.svg";
@@ -11,33 +11,51 @@ function MainSlide({ data }) {
   const [nextButton, setNextButton] = useState(false);
   const [touchPosition, setTouchPosition] = useState(null);
 
-  const transform = 1024;
+  const slideWidth = 1024;
+  const sldieMargin = 20;
+  const slideCount = data && data.length;
+
+  // const isCurrentIndex =
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const makeClone = () => {};
 
   return (
-    <div className="relative slide overflow-hidden mx-auto px-20">
-      <div className=" flex items-center w-9540px">
+    <div className="mx-auto px-20 overflow-hidden relative">
+      <div className="items-center w-9540px">
         {data &&
           data.map((list) => {
             return (
               <div
-                className="max-w-screen-lg relative"
+                className="slidelist max-w-screen-lg mr-10"
                 key={list.id}
                 style={{
-                  transform: `translateX(${-transform * currentIndex}px)`,
+                  transform: `translateX(${-slideWidth * currentIndex}px)`,
                 }}
               >
-                <div className="w-full">
-                  <img src={list.img} alt={list.slide_title} />
+                <div>
+                  <h4 className="text-blue-600 font-bold border-t px-10 py-4">
+                    <a href="/">바로가기 &gt; </a>
+                  </h4>
+                </div>
+                <div className="w-full relative">
+                  <img
+                    className="absolute top-0 left-0"
+                    src={list.img}
+                    alt={list.slide_title}
+                  />
                 </div>
                 <div className="w-96 bg-white absolute bottom-10 left-10">
                   <div className="p-10">
                     <h2 className=" text-2xl font-bold">{list.slide_title}</h2>
                     <h4>{list.slide_contents}</h4>
-                  </div>
-                  <div>
-                    <h4 className="text-blue-600 font-bold border-t px-10 py-4">
-                      <a href="/">바로가기 &gt; </a>
-                    </h4>
                   </div>
                 </div>
               </div>
@@ -46,17 +64,13 @@ function MainSlide({ data }) {
         <div className="bg w-10">
           <button
             className="flex top-24 left-[calc(100%-1350px)] justify-center items-center absolute w-10 h-20 bg-slate-200 rounded-xl overflow-hidden opacity-1"
-            onClick={() => {
-              setCurrentIndex((prevIndex) => prevIndex - 1);
-            }}
+            onClick={prevSlide}
           >
             <img className="leftArrowButton" src={LeftArrow} alt="왼쪽화살표" />
           </button>
           <button
             className="flex top-24 right-[calc(100%-1350px)] justify-center items-center absolute w-10 h-20 bg-slate-200 rounded-xl overflow-hidden opacity-1"
-            onClick={() => {
-              setCurrentIndex((prevIndex) => prevIndex + 1);
-            }}
+            onClick={nextSlide}
           >
             <img
               className="rightArrowButton"
